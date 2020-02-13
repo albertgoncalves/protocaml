a3069d4853f7ca3fb2e86db264b86a141070ef2d
Albert Goncalves <goncalves.albert@gmail.com>
2020-02-13 00:56:53 -0500

    classic

--- src/linked_list.ml
+++ src/linked_list.ml
@@ -1,42 +1,83 @@
 module LinkedList = struct
-    type 'a t = {value : 'a; mutable pointer : 'a t option ref}
+    type 'a _node = {
+        value : 'a;
+        mutable next : 'a _node ref option;
+    }
 
-    let push (x : 'a) (xs : 'a t) : unit =
-        let ptr : 'a t option = !(xs.pointer) in
-        xs.pointer := Some ({
-            value = x;
-            pointer = ref ptr;
-        })
+    and 'a _node_t = 'a _node ref option
 
-    let pop (xs : 'a t) : ('a * 'a t option) =
-        match !(xs.pointer) with
-            | None -> (xs.value, None)
-            | Some ptr -> (xs.value, Some ptr)
+    type 'a t = {
+        mutable nodes : 'a _node_t;
+    }
 
-    let print (to_string : 'a -> string) (xs : 'a t) : unit =
-        Printf.fprintf stdout "LinkedList (";
-        let rec f (xs : 'a t) : unit =
-            Printf.fprintf stdout "%s" (to_string xs.value);
-            match !(xs.pointer) with
+    let push (l : 'a t) (v : 'a) : unit =
+        let next : 'a _node ref = ref {
+            value = v;
+            next = l.nodes;
+        } in
+        l.nodes <- Some next
+
+    let pop (l : 'a t) : 'a option =
+        match l.nodes with
+            | None -> None
+            | Some ptr -> (
+                    let v : 'a = !ptr.value in
+                    l.nodes <- !ptr.next;
+                    Some v
+                )
+
+    let pop_at (l : 'a t) (i : int) : 'a option =
+        let rec f (prev : 'a _node_t) (current : 'a _node_t)
+            (next : 'a _node_t) : int -> 'a option = function
+            | 0 -> (
+                    match (prev, current, next) with
+                        | (None, Some c, (Some _ as n)) -> (
+                                l.nodes <- n;
+                                Some !c.value
+                            )
+                        | (Some p, Some c, (Some _ as n)) -> (
+                                !p.next <- n;
+                                Some !c.value
+                            )
+                        | _ -> None
+                )
+            | i -> (
+                    match (current, next) with
+                        | (Some _ as c, (Some n as n')) -> (
+                                f c n' !n.next (i - 1)
+                            )
+                        | _ -> None
+                ) in
+        match l.nodes with
+            | None -> None
+            | Some ptr -> f None (Some ptr) !ptr.next i
+
+    let print (to_string : 'a -> string) (l : 'a t) : unit =
+        Printf.fprintf stdout "LinkedList.t        : [";
+        let rec f (n : 'a _node_t) : unit =
+            match n with
                 | None -> ()
                 | Some ptr -> (
-                        Printf.fprintf stdout ", ";
-                        f ptr
+                        Printf.fprintf stdout " %s" (to_string !ptr.value);
+                        f !ptr.next
                     ) in
-        f xs;
-        Printf.fprintf stdout ")\n"
+        f l.nodes;
+        Printf.fprintf stdout "]\n"
 end
 
 let () : unit =
-    let xs : int LinkedList.t = {
-        LinkedList.value = 0;
-        LinkedList.pointer = ref None;
-    } in
-    LinkedList.push 1 xs;
-    LinkedList.push 2 xs;
-    LinkedList.push 3 xs;
-    let (x, xs) : (int * int LinkedList.t option) = LinkedList.pop xs in
-    (match xs with
-        | None -> ()
-        | Some xs -> LinkedList.print string_of_int xs);
-    Printf.fprintf stdout "%d\n%!" x
+    let l : int LinkedList.t = {LinkedList.nodes = None} in
+    for i = 0 to 10 do
+        LinkedList.push l i
+    done;
+    (match LinkedList.pop l with
+        | None -> Printf.fprintf stdout "LinkedList.pop      : None\n"
+        | Some v -> Printf.fprintf stdout "LinkedList.pop      : Some %d\n" v);
+    (match LinkedList.pop_at l 4 with
+        | None -> Printf.fprintf stdout "LinkedList.pop_at 4 : None\n"
+        | Some v -> Printf.fprintf stdout "LinkedList.pop_at 4 : Some %d\n" v);
+    (match LinkedList.pop_at l 9 with
+        | None -> Printf.fprintf stdout "LinkedList.pop_at 9 : None\n"
+        | Some v -> Printf.fprintf stdout "LinkedList.pop_at 9 : Some %d\n" v);
+    LinkedList.print string_of_int l;
+    flush stdout
