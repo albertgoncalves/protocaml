addd78790fc540d7648cd3eb55b9c8c641844d7a
Albert Goncalves <goncalves.albert@gmail.com>
2020-05-17 16:38:06 -0400

    full impl

--- src/regex.ml
+++ src/regex.ml
@@ -17,8 +17,8 @@ module ArrayStack = struct
         }
 
     let grow (xs : 'a t) : unit =
-        let capacity = xs.capacity * 2 in
-        let contents = Array.make capacity None in
+        let capacity : int = xs.capacity * 2 in
+        let contents : 'a option array = Array.make capacity None in
         Array.blit xs.contents 0 contents 0 xs.capacity;
         xs.capacity <- capacity;
         xs.contents <- contents
@@ -44,16 +44,13 @@ module ArrayStack = struct
         else
             exit 1
 
-    let pop_opt (xs : 'a t) : 'a option =
+    let peek (xs : 'a t) : 'a =
         if xs.index <> 0 then
-            (
-                xs.index <- xs.index - 1;
-                let x : 'a option = xs.contents.(xs.index) in
-                xs.contents.(xs.index) <- None;
-                x
-            )
+            match xs.contents.(xs.index - 1) with
+                | Some x -> x
+                | None -> exit 1
         else
-            None
+            exit 1
 
     let contains (xs : 'a t) (f : 'a -> bool) : bool =
         let n : int = xs.index in
@@ -97,16 +94,13 @@ module State = struct
         epsilon_transitions = ArrayStack.make ();
     }
 
-    let add_epsilon_transition (state_from : t) (state : t) : unit =
-        ArrayStack.push state state_from.epsilon_transitions
-
     let closure (nfa : link) : link =
         let first : t = make false in
         let last : t = make true in
-        add_epsilon_transition first last;
-        add_epsilon_transition first nfa.first;
-        add_epsilon_transition nfa.last last;
-        add_epsilon_transition nfa.last nfa.first;
+        ArrayStack.push last first.epsilon_transitions;
+        ArrayStack.push nfa.first first.epsilon_transitions;
+        ArrayStack.push last nfa.last.epsilon_transitions;
+        ArrayStack.push nfa.first nfa.last.epsilon_transitions;
         nfa.last.is_end <- false;
         {
             first;
@@ -115,12 +109,12 @@ module State = struct
 
     let union (a : link) (b : link) : link =
         let first : t = make false in
-        add_epsilon_transition first a.first;
-        add_epsilon_transition first b.first;
+        ArrayStack.push a.first first.epsilon_transitions;
+        ArrayStack.push b.first first.epsilon_transitions;
         let last : t = make true in
-        add_epsilon_transition a.last last;
+        ArrayStack.push last a.last.epsilon_transitions;
         a.last.is_end <- false;
-        add_epsilon_transition b.last last;
+        ArrayStack.push last b.last.epsilon_transitions;
         b.last.is_end <- false;
         {
             first;
@@ -128,7 +122,7 @@ module State = struct
         }
 
     let concat (a : link) (b : link) : link =
-        add_epsilon_transition a.last b.first;
+        ArrayStack.push b.first a.last.epsilon_transitions;
         a.last.is_end <- false;
         {
             first = a.first;
@@ -165,7 +159,7 @@ module State = struct
             (
                 let first : t = make false in
                 let last : t = make true in
-                add_epsilon_transition first last;
+                ArrayStack.push last first.epsilon_transitions;
                 {
                     first;
                     last;
@@ -219,10 +213,76 @@ module State = struct
         ArrayStack.contains !states (fun x -> x.is_end)
 end
 
+let insert_infix (input : string) : string =
+    let n : int = String.length input in
+    let m : int = n - 1 in
+    let output : Buffer.t = Buffer.create ((n * 2) - 1) in
+    for i = 0 to n - 1 do
+        let token : char = input.[i] in
+        Buffer.add_char output token;
+        if (token = '(') || (token = '|') || (m <= i) then
+            ()
+        else
+            let peek : char = input.[i + 1] in
+            if (peek = '*') || (peek = '|') || (peek = ')') then
+                ()
+            else
+                (* NOTE: In this implementation, `.` is a concatenation
+                   operator; it is not a wildcard. *)
+                Buffer.add_char output '.'
+    done;
+    Buffer.contents output
+
+module Ops = struct
+    let table : (char, int) Hashtbl.t =
+        let table : (char, int) Hashtbl.t = Hashtbl.create 3 in
+        Hashtbl.add table '|' 0;
+        Hashtbl.add table '.' 1;
+        Hashtbl.add table '*' 2;
+        table
+
+    let precedence (op : char) : int = Hashtbl.find table op
+end
+
+let to_postfix (input : string) : string =
+    let n : int = String.length input in
+    let output : Buffer.t = Buffer.create n in
+    let stack : char ArrayStack.t = ArrayStack.make () in
+    for i = 0 to n - 1 do
+        let token : char = input.[i] in
+        if (token = '.') || (token = '|') || (token = '*') then
+            let f () : bool =
+                if stack.ArrayStack.index = 0 then
+                    false
+                else
+                    let peek : char = ArrayStack.peek stack in
+                    (peek <> '(') && (
+                        (Ops.precedence token) <= (Ops.precedence peek)
+                    ) in
+            while f () do
+                Buffer.add_char output (ArrayStack.pop stack)
+            done;
+            ArrayStack.push token stack
+        else if (token = '(') then
+            ArrayStack.push token stack
+        else if (token = ')') then
+            (
+                while (ArrayStack.peek stack) <> '(' do
+                    Buffer.add_char output (ArrayStack.pop stack)
+                done;
+                ignore (ArrayStack.pop stack)
+            )
+        else
+            Buffer.add_char output token
+    done;
+    while stack.ArrayStack.index <> 0 do
+        Buffer.add_char output (ArrayStack.pop stack)
+    done;
+    Buffer.contents output
+
 let () : unit =
-    (* NOTE: Converted from `(a|b)*c`. *)
-    let postfix_expression : string = "ab|*c." in
-    let nfa : State.link = State.to_nfa postfix_expression in
+    let expression : string = to_postfix (insert_infix "(a|b)*c") in
+    let nfa : State.link = State.to_nfa expression in
     Array.iter
         (fun x -> Printf.printf "%s\t%b\n" x (State.search nfa x))
         [|
@@ -234,6 +294,7 @@ let () : unit =
             "aabbc";
             "cc";
             "cab";
+            "cabc";
             "cba";
             "cc";
         |];
