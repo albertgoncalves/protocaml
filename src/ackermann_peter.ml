462d249d7c2a3a6ca90ce858482fb24a51c25e34
albertgoncalves <goncalves.albert@gmail.com>
2021-04-17 23:04:43 -0400

    ackermann fn

---
+++ src/ackermann_peter.ml
@@ -0,0 +1,8 @@
+let rec ackermann : (int * int) -> int = function
+    | (0, n) -> n + 1
+    | (m, 0) -> ackermann (m - 1, 1)
+    | (m, n) -> ackermann (m - 1, ackermann (m, n - 1))
+
+let () : unit =
+    ackermann (3, 9)
+    |> Printf.printf "%d\n"
