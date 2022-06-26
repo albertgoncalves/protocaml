6713c765a2856cffdc73bb25d560999774252d12
Albert Goncalves <goncalves.albert@gmail.com>
2022-06-26 13:13:42 -0400

    simple global var example

---
+++ src/global_var.ml
@@ -0,0 +1,12 @@
+let counter : int ref = ref 0
+
+let f (x : string) : string =
+    let c : int = !counter in
+    incr counter;
+    Printf.sprintf "%s%d" x c
+
+let () : unit =
+    print_endline (f "1");
+    print_endline (f "1");
+    print_endline (f "1");
+    flush stdout
